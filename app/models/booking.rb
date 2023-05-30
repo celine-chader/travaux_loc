710650c352b0619ac58350c24a84dd85654fefa9
stephanie bourdillon <bourdillon.s@gmail.com>
2023-05-30 18:08:15 +0200

    dashboard ok vérifier pending

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :ad
 
-  validates :availability, presence: true
   validates :user_id, presence: true
   validates :ad_id, presence: true
 end
