bcf8af543acfbd844c8f5f03e8dfb1262e720257
stephanie bourdillon <bourdillon.s@gmail.com>
2023-05-31 17:12:43 +0200

    price ok

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -8,7 +8,7 @@ class Ad < ApplicationRecord
   validates :description, presence: true
   validates :address, presence:true
   validates :category, inclusion: { in: CATEGORIES }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
