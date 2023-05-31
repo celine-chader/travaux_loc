68edc90dfea51043f53f69f8704852481c91b90e
d.s <serrano.david.liaca@gmail.com>
2023-05-31 15:40:59 +0200

    did migration after pull

--- db/schema.rb
+++ db/schema.rb
@@ -50,10 +50,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_104656) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-    t.integer "price"
     t.index ["user_id"], name: "index_ads_on_user_id"
   end
 
@@ -80,8 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_104656) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
