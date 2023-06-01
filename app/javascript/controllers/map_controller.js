f1df07e6f99e0cc27562fbe003d00b7555a70dd9
Amal Rabbani <130931565+AmalRabbani@users.noreply.github.com>
2023-06-01 12:24:05 +0200

    test

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -20,18 +20,18 @@ export default class extends Controller {
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
   }
-
   #addMarkersToMap() {
-    console.log(this.markersValue)
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
     })
   };
+
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   };
+
 }
