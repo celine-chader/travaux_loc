4401ca3507c987e44f20ad4d34bb2c514fa272e5
stephanie bourdillon <bourdillon.s@gmail.com>
2023-06-01 12:19:00 +0200

    form du show MAJ avec prix panier

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,8 @@ User.destroy_all
 
 puts "starting seeding"
 
+users = User.new([{ email: "celine.dion@gmail.com", password: "123456", first_name: Faker::Name.first_name, last_name:}, {}])
+
 15.times do
   user = User.new(
     email: Faker::Internet.email, password: "123456" ,first_name: Faker::Name.first_name, last_name: Faker::Name.last_name
