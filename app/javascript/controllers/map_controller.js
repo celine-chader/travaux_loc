efa0d6229765eedae6c5660de7219fddfbe5bbb8
d.s <serrano.david.liaca@gmail.com>
2023-06-02 12:24:54 +0200

    icon map show img show

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -18,25 +18,12 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v11"
     })
 
-    this.markerValue((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
-      const Marker = document.createElement("div")
-      Marker.innerHTML = marker.marker_html
-      new mapboxgl.Marker(Marker)
+     const customMarker = document.createElement("div")
+     customMarker.innerHTML = this.markerValue.marker_html
+      new mapboxgl.Marker(customMarker)
         .setLngLat([ this.markerValue.lng, this.markerValue.lat ])
         .addTo(this.map)
 
-    });
-
-      // this.markerValue((marker) => {
-      //   const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
-      //   const customMarker = document.createElement("div")
-      //   customMarker.innerHTML = marker.marker_html
-      //   new mapboxgl.Marker(customMarker)
-      //     .setLngLat([ marker.lng, marker.lat ])
-      //     .setPopup(popup) // Add this
-      //     .addTo(this.map)
-      // });
     const bounds = new mapboxgl.LngLatBounds()
     bounds.extend([ this.markerValue.lng, this.markerValue.lat ])
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
